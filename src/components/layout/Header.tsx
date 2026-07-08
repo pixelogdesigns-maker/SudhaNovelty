6a3e7730d2e229e1855afe260a45448c7a7d800c
Wix Remote Machine <support@wix.com>
2026-07-08 10:34:33 +0000

    Adding Meta Pixel code

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { Image } from '@/components/ui/image';
 import { Menu, X, ShoppingCart } from 'lucide-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import ModernCart from '@/components/ecom/ModernCart';
 import { useCart } from '@/integrations';
@@ -9,6 +9,35 @@ function Header() {
   const location = useLocation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const { itemCount, actions } = useCart();
+
+  // Initialize Meta Pixel
+  useEffect(() => {
+    // Load Meta Pixel script
+    const script = document.createElement('script');
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '5242827002609560');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+
+    // Add noscript image
+    const noscript = document.createElement('noscript');
+    const img = document.createElement('img');
+    img.height = 1;
+    img.width = 1;
+    img.style.display = 'none';
+    img.src = 'https://www.facebook.com/tr?id=5242827002609560&ev=PageView&noscript=1';
+    noscript.appendChild(img);
+    document.head.appendChild(noscript);
+  }, []);
 
   const navLinks = [
     { name: 'Home', path: '/' },
